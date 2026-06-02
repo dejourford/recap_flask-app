26665cb120f68a91e58e1465a78b57917c02f0b2
dejourford <dejourford@yahoo.com>
2026-06-02 14:15:15 +0000

    update index path

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello from CI/CD Piple!@!!!'
+    return 'CI/CD is working my boy! you movigngnggg frfr!@!!!'
 
 @app.route('/about')
 def about():
